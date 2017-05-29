c7487357272de0b76fe578da6e7d90430ddf78c1
TaPO4eg3D <fo.denis2011@gmail.com>
2017-05-29 18:58:03 +0700

    added new project

---
+++ portfolio1/src/app/js/common.js
@@ -0,0 +1,13 @@
+$(function() {
+
+	$("#menu-close").click(function(e) {
+			e.preventDefault();
+			$("#sidebar-wrapper").toggleClass("active");
+	});
+	// Opens the sidebar menu
+	$("#menu-toggle").click(function(e) {
+			e.preventDefault();
+			$("#sidebar-wrapper").toggleClass("active");
+	});
+
+});
---
+++ portfolio1/src/readme.md
@@ -0,0 +1,3 @@
+<h1>OptimizedHTML - Start HTML Template</h1>
+
+Start template for convenient web-development.
